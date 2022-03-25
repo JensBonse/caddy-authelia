4aa5c9eddcbc28746b9af9baa6d50d044f7fb466
jb-home <15381432+jb-home@users.noreply.github.com>
2022-03-25 14:02:49 +0100

    Update get-version.sh

--- get-version.sh
+++ get-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VERSIONS=version.properties
 (
-  echo CADDY_VERSION=\"$(curl --silent https://api.github.com/repos/caddyserver/caddy/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')\"
+  echo CADDY_VERSION=\"$(wget -qO - https://api.github.com/repos/caddyserver/caddy/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')\"
 ) | tee $VERSIONS
